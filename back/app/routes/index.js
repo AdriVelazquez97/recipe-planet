28b86d601a66b319d197c5129f3298b3130672d6
Adrian <adrivelazquez97@gmail.com>
2019-10-28 17:44:46 +0000

    Added checkToken to the routes

--- back/app/routes/index.js
+++ back/app/routes/index.js
@@ -1,11 +1,13 @@
 const router = require('express').Router();
 
+const { checkToken } = require('../middlewares/index')
+
 const userRouter = require('./userRouter');
 const recipeRouter = require('./recipeRouter');
 const authRouter = require('./authController');
 
 router.use('/auth', authRouter);
-router.use('/users', userRouter);
-router.use('/recipe', recipeRouter);
+router.use('/users', checkToken, userRouter);
+router.use('/recipe', checkToken, recipeRouter);
 
 module.exports = router;
