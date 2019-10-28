b159bb3432074f6dba7e611c8141d61eba4af1fd
Adrian <adrivelazquez97@gmail.com>
2019-10-28 17:34:03 +0000

    Refactor auth router

--- back/app/routes/index.js
+++ back/app/routes/index.js
@@ -2,8 +2,9 @@ const router = require('express').Router();
 
 const userRouter = require('./userRouter');
 const recipeRouter = require('./recipeRouter');
+const authRouter = require('./authController');
 
-
+router.use('/auth', authRouter);
 router.use('/users', userRouter);
 router.use('/recipe', recipeRouter);
 
