8e45061f07c115521399ee2f38832180b74ae88b
Adrian <adrivelazque97@gmail.com>
2019-11-22 18:22:09 +0000

    WIP

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,11 +1,13 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import About from "@/views/About.vue";
+import Home from "@/views/Home.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
+    // redirect to main page, si no estas logueado pues mandarlo al login
     path: "/",
     name: "home",
     component: Home
@@ -13,11 +15,7 @@ const routes = [
   {
     path: "/about",
     name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    component: About
   }
 ];
 
